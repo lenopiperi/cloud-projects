b1678eda87531138011e6b6a5b0ddaa8cfb377dd
lenopiperi <42346651+lenopiperi@users.noreply.github.com>
2019-08-25 12:29:07 -0400

    going back to basics

--- read-write-cloudstorage/main.py
+++ read-write-cloudstorage/main.py
@@ -1,43 +1,35 @@
-import logging
-import os
-from app import app
-import cloudstorage as gcs
-import webapp2
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from google.appengine.api import app_identity
-
-def get(self):
-  bucket_name = os.environ.get('BUCKET_NAME',
-                               app_identity.get_default_gcs_bucket_name())
-
-  self.response.headers['Content-Type'] = 'text/plain'
-  self.response.write('Demo GCS Application running from Version: '
-                      + os.environ['CURRENT_VERSION_ID'] + '\n')
-  self.response.write('Using bucket name: ' + bucket_name + '\n\n')
+# [START gae_python37_app]
+from flask import Flask
 
 
-  def create_file(self, filename):
-  # """Create a file.
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
 
-  # The retry_params specified in the open call will override the default
-  # retry params for this particular file handle.
 
-  # Args:
-  #   filename: filename.
-  # """
-	  self.response.write('Creating file %s\n' % filename)
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
-	  write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-	  gcs_file = gcs.open(filename,
-	                      'w',
-	                      content_type='text/plain',
-	                      options={'x-goog-meta-foo': 'foo',
-	                               'x-goog-meta-bar': 'bar'},
-	                      retry_params=write_retry_params)
-	  gcs_file.write('abcde\n')
-	  gcs_file.write('f'*1024*4 + '\n')
-	  gcs_file.close()
-	  self.tmp_filenames_to_clean_up.append(filename)
 
-  if __name__ == '__main__':
-  	app.run()
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
